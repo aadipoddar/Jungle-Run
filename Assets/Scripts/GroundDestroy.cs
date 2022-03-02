6d5f1c37706b68c689dba6673b1bc45f1719d10f
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-03 02:31:15 +0530

    Destroying Grounds

---
+++ Assets/Scripts/GroundDestroy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroy : MonoBehaviour
+{
+
+    private GameObject point;
+
+    void Start()
+    {
+        point = GameObject.Find("GroundEndPoint");
+    }
+
+    void Update()
+    {
+        if (transform.position.x < point.transform.position.x)
+            gameObject.SetActive(false);
+    }
+}
