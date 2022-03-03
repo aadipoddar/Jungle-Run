6409870a557303641f3db5d90c9c1ed946e53966
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-03 16:42:22 +0530

    Increasing Player Speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,15 +14,28 @@ public class Player : MonoBehaviour
     public AudioSource deathSound;
     public AudioSource jumpSound;
 
+    public float mileStone;
+    private float mileStoneCount;
+    public float speedMultiplier;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+
+        mileStoneCount = mileStone;
     }
 
     void Update()
     {
+        if (transform.position.x > mileStoneCount)
+        {
+            mileStoneCount += mileStone;
+            speed *= speedMultiplier;
+            mileStone += mileStone * speedMultiplier;
+        }
+
         rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
 
         bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
