8c64eddb5c8f68a883a619e92ba93ad2a4e736cf
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-03 15:12:51 +0530

    Scrolling Background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public Renderer background;
+    public float backgroundSpeed;
+
+    void Update()
+    {
+        background.material.mainTextureOffset += new Vector2(backgroundSpeed * Time.deltaTime, 0f);
+    }
+}
